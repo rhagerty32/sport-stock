781e94b294ddd312f8b0d48773ef333ed3e2035d
rhagerty32 <ry2402@gmail.com>
2025-10-21 22:44:58 -0600

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,30 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <NativeTabs>
+                <NativeTabs.Trigger name="index">
+                    <Icon sf="house.fill" />
+                    <Label>Home</Label>
+                </NativeTabs.Trigger>
+                <NativeTabs.Trigger name="explore">
+                    <Icon sf="paperplane.fill" />
+                    <Label>Explore</Label>
+                </NativeTabs.Trigger>
+                <NativeTabs.Trigger name="profile">
+                    <Icon sf="person.fill" />
+                    <Label>Profile</Label>
+                </NativeTabs.Trigger>
+            </NativeTabs>
+            <StatusBar style="auto" />
+        </ThemeProvider>
+    );
 }
